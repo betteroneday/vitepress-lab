844c08d29a99768e48832050879f3c85c8799cc2
betteroneday <betteroneday@users.noreply.github.com>
2022-11-10 09:18:47 +0000

    deploy: 1dc6496af98f8465ac502979d1000c8e10f53437

---
+++ assets/guide_install.md.62dcea72.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as a,a as s}from"./app.8a131e5e.js";const f=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"guide/install.md","lastUpdated":1668071863000}'),n={name:"guide/install.md"},o=s("p",null,"install",-1),l=[o];function r(c,i,d,_,p,m){return a(),e("div",null,l)}const h=t(n,[["render",r]]);export{f as __pageData,h as default};
